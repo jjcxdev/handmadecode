923dd8636394d38aad1f1b914a4df3e89e9b8309
jjcx <112969641+jjcxdev@users.noreply.github.com>
2023-10-27 04:00:37 -0400

    added button to cta

--- src/pages/cta.tsx
+++ src/pages/cta.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button } from "@/components/button";
 
 export default function CTA() {
   return (
@@ -25,6 +26,9 @@ const Content = () => (
   <div className="flex h-full w-full flex-col items-end justify-between px-4 pt-20 md:px-24 md:pt-48">
     <div className="flex h-fit w-full max-w-[100rem] flex-wrap text-center font-aurora text-[9vw] leading-none text-neutral-50 md:text-[4vw]">
       Contact us for Web Design that&apos;s ahead of its time.
+    </div>
+    <div className="flex h-full w-full items-end justify-center pb-10">
+      <Button title="Send us a fax" />
     </div>
   </div>
 );
