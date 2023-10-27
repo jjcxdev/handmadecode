de1c0616427d61635fc140cb639b22f9b7048247
jjcx <112969641+jjcxdev@users.noreply.github.com>
2023-10-27 04:03:23 -0400

    typo fixed

--- src/pages/clients.tsx
+++ src/pages/clients.tsx
@@ -26,7 +26,7 @@ const ClientsContent = () => (
   <div className="flex h-full w-full flex-col items-end justify-between p-4 pt-20 md:justify-center md:p-24">
     <div>
       <div className="h-fit w-full font-aurora text-[9vw] leading-none text-neutral-100 md:max-w-4xl md:text-[4vw]">
-        Rave reviews on the CB radio. d
+        Rave reviews on the CB radio.
       </div>
       <div className="h-fit w-full py-2 font-deco text-[4vw] leading-none text-neutral-100 md:block md:max-w-4xl md:text-[1.5vw]">
         Handmade Code -- sharper than a laserdisc!
