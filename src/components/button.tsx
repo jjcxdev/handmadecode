410c3762f6819c68c38d102ece57af01909115a1
jjcx <112969641+jjcxdev@users.noreply.github.com>
2023-11-03 03:00:11 -0400

    buttons are ugly Fixes #17

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -6,7 +6,7 @@ interface ButtonProps {
 
 export const Button: React.FC<ButtonProps> = ({ title }) => {
   return (
-    <div className="flex h-8 w-fit items-center justify-center rounded-lg border-4 border-orange-700 bg-[#3E2C2B] px-4 py-6 font-aurora text-[6vw] text-neutral-100">
+    <div className="flex w-fit max-w-xs items-center justify-center rounded-lg border-2 border-neutral-100 bg-neutral-50/20 px-4 py-2 font-aurora text-2xl text-neutral-100 transition  duration-300 hover:border-orange-500/50 hover:text-neutral-700 ">
       {title}
     </div>
   );
