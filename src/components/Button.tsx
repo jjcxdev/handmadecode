8a418d94c5aacf6d5f766b3846f9f976a35932ed
jjcx <112969641+jjcxdev@users.noreply.github.com>
2023-11-04 05:14:30 -0400

    buttons Fixes #22

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ interface ButtonProps {
 
 export const Button: React.FC<ButtonProps> = ({ title }) => {
   return (
-    <div className="flex w-fit max-w-xs items-center justify-center rounded-lg border-2 border-neutral-100 bg-neutral-50/20 px-4 py-2 font-aurora text-2xl text-neutral-100 transition  duration-300 hover:border-orange-500/50 hover:text-neutral-700 ">
+    <div className="flex w-fit max-w-xs items-center justify-center rounded-lg border-2 border-neutral-100 bg-transparent px-4 py-2 font-aurora text-xl text-neutral-50 transition  duration-300 hover:border-orange-400 hover:bg-orange-400/50 ">
       {title}
     </div>
   );
